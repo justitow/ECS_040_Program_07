b40df1a5faf5f50785a9492e8ac792ac1fd4e998
justitow <justitow@gmail.com>
2016-02-29 22:26:23 -0800

    rename

--- decipher.cpp
+++ decipher.cpp
@@ -25,6 +25,8 @@ int main(int argc, const char * argv[]) {
 	std::vector<float> total_chi(27, 0);
 	std::vector<float> square(26, 0);
 	std::vector<float> total_square(27, 0);
+	std::vector<float> copy(26,0);
+	std::vector<int> six_index(6, 0);
 	
 	std::ifstream inf(argv[1]);
 	std::stringstream buffer;
@@ -81,26 +83,40 @@ int main(int argc, const char * argv[]) {
 	}
 	total_chi.at(0) = total_chi.at(26);
 	total_square.at(0) = total_chi.at(26);
+	total_chi.erase(total_chi.begin() + 25);
+	total_square.erase(total_square.begin() + 25);
 	
-	for (int i = 0; i < 26; i++)
+	copy = total_square;
+	std::sort(copy.begin(), copy.end());
+	for (int i = 0; i < 6; i++)
 	{
-		std::cout << "shift " << i << " Chi: " << total_chi.at(i) << " Square: " << total_square.at(i) <<  std::endl;
+		six_index.at(i) = (int)(find(total_square.begin(), total_square.end(), copy.at(25 - i)) - total_square.begin());
+		//std::cout << pos << " ";
+	}
+	
+	int count = 1;
+	for (std::vector<int>::iterator pos = six_index.begin(); pos < six_index.end(); pos++)
+	{
+		//std::cout << "shift " << *pos << " Chi: " << total_chi.at(*pos) << " Square: " << total_square.at(*pos) <<  std::endl;
+		std::cout << "Deciphered message version #" << count << std::endl;
 		for (std::string::iterator it = str.begin(); it < str.end(); it++)
 		{
 			if (std::toupper(*it) >= 'A' && std::toupper(*it) <= 'Z')
 			{
-				if (std::toupper(*it) + i > 'Z')
+				if (std::toupper(*it) + *pos > 'Z')
 				{
-					std::cout << (char)(*it + i - 26);
+					std::cout << (char)(*it + *pos - 26);
 				}
 				else
 				{
-					std::cout << (char)(*it + i );
+					std::cout << (char)(*it + *pos );
 				}
 			}
 			else
 				std::cout << (char)(*it);
 		}
+		std::cout << std::endl;
+		count++;
 	}
 	
 	return 0;
